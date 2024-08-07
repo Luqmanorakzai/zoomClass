ca3816a1e9e8176ddd4289e42b7bba879801de3f
luqmanorakzai <luqmanhakim577577@gmail.com>
2024-08-07 21:20:20 +0500

    Stack and cumtom app bar

--- lib/screens/stack_screen.dart
+++ lib/screens/stack_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom/widget/custom_app_bar.dart';
-
 class StackScreen extends StatelessWidget {
   const StackScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
